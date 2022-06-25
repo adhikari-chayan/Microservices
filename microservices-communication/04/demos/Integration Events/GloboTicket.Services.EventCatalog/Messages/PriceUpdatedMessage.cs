e97d74765e22754cbd72090ea9f9f16f58e3e90b
Chayan Adhikari <chayan.adhikari@ivycomptech.com>
2022-06-26 02:28:36 +0530

    adding communication sample

---
+++ microservices-communication/04/demos/Integration Events/GloboTicket.Services.EventCatalog/Messages/PriceUpdatedMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using GloboTicket.Integration.Messages;
+
+namespace GloboTicket.Services.EventCatalog.Messages
+{
+    public class PriceUpdatedMessage: IntegrationBaseMessage
+    {
+        public Guid EventId { get; set; }
+        public int Price { get; set; }
+    }
+}
